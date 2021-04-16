5f5defd95ee9f06715f6ef76d026e3362b7bc287
nupaT <trash_jack@mail.ru>
2021-04-16 16:44:08 +0300

    fix: easy bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Profile from './components/Content/Profile';
 import Header from './components/Header';
