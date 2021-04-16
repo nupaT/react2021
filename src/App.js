61183b6fdcc3545138d6d7f00d447735263521c4
nupaT <trash_jack@mail.ru>
2021-04-16 12:40:41 +0300

    remove: List1 Nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import List1 from './List1';
-import Nav from './Nav';
-
 
 
 const App1 = () => {
   return (
     <div>
-     <Nav />
-     <List1 />
+
     </div>
   );
 }
