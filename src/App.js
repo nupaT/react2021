ed86afeec44f00d061bb8aa78a4a97ec75474ab7
nupaT <trash_jack@mail.ru>
2021-04-16 13:02:25 +0300

    add: basic structure document

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-const App1 = () => {
+const App = () => {
   return (
-    <div>
 
+    <div className="bodyBlock">
+      <div className="headerBlock">
+        Header
+      </div>
+
+      <div className="mainBlock">
+        <div className="nav__menu">
+          NavMenu
+        </div>
+        <div className="content">
+          Content
+        </div>
+      </div>
     </div>
+
   );
 }
 
-export default App1;
+export default App;
 
