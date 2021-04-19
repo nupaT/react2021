f53fb91be6a562fed1f59e62180d3566a796366d
nupaT <trash_jack@mail.ru>
2021-04-19 13:40:22 +0300

    add: first css-module navMenu. Change: navMenu.jsk for css-module

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import './NavMenu.css';
+import classes from './NavMenu.module.css';
 
 
 const NavMenu = () => {
   return (
-    <div className="nav__menu">
-          <ul className="nav__menu_list">
-            <li className="nav__menu_item"><a href="#!">Profile</a></li>
-            <li className="nav__menu_item"><a href="#!">News</a></li>
-            <li className="nav__menu_item"><a href="#!">Messages</a></li>
-            <li className="nav__menu_item"><a href="#!">Music</a></li>
-            <li className="nav__menu_item"><a href="#!">Settings</a></li>
-          </ul>
-        </div>
-  ) 
+    <div className={classes.menu}>
+      <ul className={classes.list}>
+        <li className={classes.item}><a href="#!">Profile</a></li>
+        <li className={classes.item}><a href="#!">News</a></li>
+        <li className={classes.item}><a href="#!">Messages</a></li>
+        <li className={classes.item}><a href="#!">Music</a></li>
+        <li className={classes.item}><a href="#!">Settings</a></li>
+      </ul>
+    </div>
+  )
 }
 
 export default NavMenu;
