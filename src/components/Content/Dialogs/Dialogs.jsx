6cc81fcea1bc01f0652ed3f84454b6a59985714c
nupaT <trash_jack@mail.ru>
2021-04-22 15:58:10 +0300

    modifi: dialogs

--- src/components/Content/Dialogs/Dialogs.jsx
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -2,33 +2,39 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import classes from './Dialogs.module.css';
 
+const Users = (props) => {
+  return (
+    <div className={`${classes.list} ${classes.user}`}>
+      <NavLink activeClassName={classes.active} to="/Dialogs/User1">props.name</NavLink>
+    </div>
+  );
+}
+
 const Dialogs = (props) => {
   return (
     <div className={classes.block}>
-      <div className={classes.title}> 
+      <div className={classes.title}>
         Dialogs
         </div>
-      <div className={classes.mainDialogs}> 
+      <div className={classes.mainDialogs}>
         <div className={classes.users}>
-            <div className={`${classes.list} ${classes.user}`}>
-              <NavLink activeClassName={classes.active} to="/Dialogs/User1">User1</NavLink>
-            </div>
-            <div className={`${classes.list} ${classes.user}`}>
-              <NavLink activeClassName={classes.active} to="/Dialogs/User2">User2</NavLink>
-            </div>
-            <div className={`${classes.list} ${classes.user}`}>
-              <NavLink activeClassName={classes.active} to="/Dialogs/User3">User3</NavLink>
-            </div>
+          <Users name="user1" />
+          <div className={`${classes.list} ${classes.user}`}>
+            <NavLink activeClassName={classes.active} to="/Dialogs/User2">User2</NavLink>
+          </div>
+          <div className={`${classes.list} ${classes.user}`}>
+            <NavLink activeClassName={classes.active} to="/Dialogs/User3">User3</NavLink>
+          </div>
         </div>
         <div className={classes.messages}>
-            <div className={`${classes.list} ${classes.mess}`}>
-              Mess1
+          <div className={`${classes.list} ${classes.mess}`}>
+            Mess1
             </div>
-            <div className={`${classes.list} ${classes.mess}`}>
-              Mess2
+          <div className={`${classes.list} ${classes.mess}`}>
+            Mess2
             </div>
-            <div className={`${classes.list} ${classes.mess}`}>
-              Mess3
+          <div className={`${classes.list} ${classes.mess}`}>
+            Mess3
             </div>
         </div>
 
