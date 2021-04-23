411a24d61ec0bc7bd47a3fa345f1fc0d880ac3b3
nupaT <trash_jack@mail.ru>
2021-04-23 13:03:06 +0300

    add: refactoring messages users and profile info

--- src/components/Content/Dialogs/Dialogs.jsx
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -5,18 +5,34 @@ import classes from './Dialogs.module.css';
 const Users = (props) => {
   return (
     <div className={`${classes.list} ${classes.user}`}>
-      <NavLink activeClassName={classes.active} to={"/Dialogs/" + props.name}>{props.name}</NavLink>
+      <NavLink activeClassName={classes.active} to={"/Dialogs/" + props.id}>{props.name}</NavLink>
     </div>
   );
 }
 
 const Message = (props) => {
   return (
-    <div className={`${classes.list} ${classes.mess}`}>
-      {props.mess}
+    <div className={`${classes.list} ${classes.message}`}>
+      {props.message}
     </div>
   );
 }
+
+let usersData = [
+  { id: 1, name: 'user1' },
+  { id: 2, name: 'user2' },
+  { id: 3, name: 'user3' },
+  { id: 4, name: 'user4' },
+  { id: 5, name: 'user5' },
+];
+
+let messagesData = [
+  { id: 1, message: 'Message 1' },
+  { id: 2, message: 'Message 2' },
+  { id: 3, message: 'Message 3' },
+  { id: 4, message: 'Message 4' },
+  { id: 5, message: 'Message 5' },
+];
 
 const Dialogs = (props) => {
   return (
@@ -26,14 +42,16 @@ const Dialogs = (props) => {
         </div>
       <div className={classes.mainDialogs}>
         <div className={classes.users}>
-          <Users name="User1" />
-          <Users name="User2" />
-          <Users name="User3" />
+          <Users name="User1" id="1" />
+          <Users name="User2" id="2" />
+          <Users name="User3" id="3" />
+          <Users name="User4" id="4" />
+          <Users name="User5" id="5" />
         </div>
         <div className={classes.messages}>
-          <Message mess="Hi" />
-          <Message mess="Mess2" />
-          <Message mess="Mess3" />
+          <Message message="Hi" />
+          <Message message="Mess2" />
+          <Message message="Mess3" />
         </div>
       </div>
     </div>
--- src/components/Content/MyPosts/MyPosts.jsx
+++ src/components/Content/MyPosts/MyPosts.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import MyPost from './MyPost/MyPost';
 import classes from './MyPosts.module.css';
 
+let postsData = [
+  { id: 1, message: 'Hi, howe are you?', likesCount: 2 },
+  { id: 2, message: "Hello! I'm fine!", likesCount: 12 },
+  { id: 3, message: 'Me too!', likesCount: 18 }
+]
+
 const MyPosts = () => {
   return (
     <div className={classes.block}>
@@ -10,8 +16,8 @@ const MyPosts = () => {
         <textarea className={classes.input} type="text" placeholder="Введите ваше сообщение"></textarea>
         <div className={classes.submit__block}><input className={classes.submit} type="submit" name="post" value="Тык"></input></div>
       </div>
-      <MyPost message="Hi, how are you?" likescount="2"/>
-      <MyPost message="Hello! I'm fine!" likescount="10"/>
+      <MyPost message={postsData[0].message} likesCount={postsData[0].likesCount} />
+      <MyPost message={postsData[1].message} likesCount={postsData[1].likesCount} />
     </div>
   );
 }
