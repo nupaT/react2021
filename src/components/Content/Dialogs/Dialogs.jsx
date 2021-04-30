6c12a643b44f03320394e6bcf0dddd2cf43fbcc4
nupaT <trash_jack@mail.ru>
2021-04-30 14:04:53 +0300

    add: alert for submit button in dialogs; style for added elements

--- src/components/Content/Dialogs/Dialogs.jsx
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -1,34 +1,37 @@
-import React from 'react';
-import classes from './Dialogs.module.css';
-import Message from './Message/Message';
-import Users from './Users/Users';
-
-
-
+import React from "react";
+import classes from "./Dialogs.module.css";
+import Message from "./Message/Message";
+import Users from "./Users/Users";
 
 const Dialogs = (props) => {
+  let usersElements = props.usersData.map((user) => <Users name={user.name} id={user.id} />);
 
-  let usersElements = 
-  props.usersData.map(user => <Users name={user.name} id={user.id} />)
+  let messagesElements = props.messagesData.map((mess) => <Message message={mess.message} />);
 
-  let messagesElements = 
-  props.messagesData.map(mess => <Message message={mess.message} />)
+  let messageText = React.createRef();
+
+  let addMessage = () => {
+    let text = messageText.current.value;
+    alert(text);
+  };
 
   return (
     <div className={classes.block}>
-      <div className={classes.title}>
-        Dialogs
-        </div>
+      <div className={classes.title}>Dialogs</div>
       <div className={classes.mainDialogs}>
-        <div className={classes.users}>
-          {usersElements}
-        </div>
+        <div className={classes.users}>{usersElements}</div>
         <div className={classes.messages}>
-          {messagesElements}
+          <div>{messagesElements}</div>
+          <div className={classes.message__add}>
+            <div className={classes.message__textarea}>
+              <textarea className={classes.input} ref={messageText} type="text" placeholder="Ваше сообщение..."></textarea>
+            </div>
+            <input className={classes.submit} onClick={addMessage} type="submit" name="dialogPost" value="Отправить"></input>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Dialogs;
