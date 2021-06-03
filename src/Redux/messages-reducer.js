0dd7c9e4dd6c97c689069e5f3817929c47e0d54d
nupaT <trash_jack@mail.ru>
2021-06-03 13:55:26 +0300

    less: 41. add: profile-reducer, messages-reducer

---
+++ src/Redux/messages-reducer.js
@@ -0,0 +1,23 @@
+const ADD_MESSAGE = "ADD-MESSAGE";
+const CHANGE_MESSAGE_TEXT = "CHANGE-MESSAGE-TEXT";
+
+const messagesReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_MESSAGE:
+      let newMess = {
+        id: state.messagesData.length + 1,
+        message: state.textMessage,
+      };
+      state.messagesData.push(newMess);
+      state.textMessage = "";
+      break;
+    //изменение текста в поле ввода сообщения
+    case CHANGE_MESSAGE_TEXT:
+      state.textMessage = action.messageText;
+      break;
+
+    default:
+      break;
+  }
+  return state;
+};
---
+++ src/Redux/profile-reducer.js
@@ -0,0 +1,25 @@
+const ADD_POST = "ADD-POST";
+const CHANGE_POST_TEXT = "CHANGE-POST-TEXT";
+
+const profileReducer = (state, action) => {
+  switch (action.type) {
+    //добавление поста
+    case ADD_POST:
+      let newPost = {
+        id: state.postsData.length + 1,
+        message: state.textPost,
+        likesCount: 0,
+      };
+      state.postsData.push(newPost);
+      state.textPost = ""; //обнуление поля ввода после отправки
+      break;
+    //изменение текста в поле ввода поста
+    case CHANGE_POST_TEXT:
+      state.textPost = action.postText;
+      break;
+
+    default:
+      break;
+  }
+  return state;
+};
