970d2b51bc52d23ea7675de4a2b532a0810bab37
nupaT <trash_jack@mail.ru>
2021-06-09 12:00:08 +0300

    less: 43. remove: _State from Store.js

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,40 +2,6 @@ import messagesReducer from "./messages-reducer";
 import profileReducer from "./profile-reducer";
 
 let Store = {
-  _State: {
-    profilePage: {
-      postsData: [
-        { id: 1, message: "Hi, howe are you?", likesCount: 2 },
-        { id: 2, message: "Hello! I'm fine!", likesCount: 12 },
-        { id: 3, message: "Me too!", likesCount: 18 },
-        { id: 4, message: "Go!", likesCount: 38 },
-      ],
-
-      textPost: "",
-    },
-
-    messagePage: {
-      usersData: [
-        { id: 1, name: "User1" },
-        { id: 2, name: "User2" },
-        { id: 3, name: "User3" },
-        { id: 4, name: "User4" },
-        { id: 5, name: "User5" },
-        { id: 6, name: "User6" },
-      ],
-
-      messagesData: [
-        { id: 1, message: "Message 1" },
-        { id: 2, message: "Message 2" },
-        { id: 3, message: "Message 3" },
-        { id: 4, message: "Message 4" },
-        { id: 5, message: "Message 5" },
-        { id: 6, message: "Message 6" },
-      ],
-
-      textMessage: "",
-    },
-  },
   _rerenderMainPage() {},
   //забирает значение State для внешнего использования (правильное использование данных
   // обращаемся из самого объекта к данным а не из вне)
