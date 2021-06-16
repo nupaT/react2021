c73f7cf3ea35787acd3c2ef255b068a97ef657d0
nupaT <trash_jack@mail.ru>
2021-06-16 14:05:33 +0300

    less: 47; Change: profile info

--- src/components/Content/UserInfo/UserDesc/UserDesc.jsx
+++ src/components/Content/UserInfo/UserDesc/UserDesc.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import classes from './UserDesc.module.css';
-
+import React from "react";
+import classes from "./UserDesc.module.css";
 
 const UserDesc = () => {
   return (
-      <div className={classes.user__info}>
-        <div className={classes.name}>
-          Evgeni A.
+    <div className={classes.user__info}>
+      <div className={classes.name}>Evgeni A.</div>
+      <div className={classes.desc}>
+        <ul className={classes.list}>
+          <li className={classes.item}>Date of Birth: June 8</li>
+          <li className={classes.item}>City: Vitebsk</li>
+          <li className={classes.item}>Education: Magistr of cat affairs</li>
+        </ul>
       </div>
-        <div className={classes.desc}>
-          <ul className={classes.list} >
-            <li className={classes.item}>Date of Birth: 8 juine</li>
-            <li className={classes.item}>City: Vitebsk</li>
-            <li className={classes.item}>Education: Magistr of cat affairs</li>
-          </ul>
-        </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
 export default UserDesc;
