8c95a34301d59670e73a565eb2664779abc0f392
nupaT <trash_jack@mail.ru>
2021-06-24 13:14:31 +0300

    less: 53; del: User.jsx - finctional component; add: User.jsx - class component

--- src/components/Content/UsersData/Users.jsx
+++ src/components/Content/UsersData/Users.jsx
@@ -3,69 +3,72 @@ import React from "react";
 import classes from "./Users.module.css";
 import userDefaultAva from "../../../image/default_ava.jpg";
 
-const Users = (props) => {
-  // debugger;
-  //вызывается setUsers 2 раза!!!! еще не разобрался почему
-  //23.06.21 всеравно попадает 2 раза!!
-  //Решено: убираем React.StrictMode из index.js - по умолчанию рендерит повторно компонент
-  //отсюда двойная отрисовка списка юзверей - Оставлю пока тут
-  let getUsers = () => {
-    if (props.user.length === 0) {
+//создаем классовую компоненту
+//даем ей метод getUser
+//и обязательный метод render для отрисовки jsx
+//теперь чтобы отратиться к props нужно добавлять this т.к.
+//пропсы теперь являются частью класса
+
+class Users extends React.Component {
+  getUsers = () => {
+    if (this.props.user.length === 0) {
       console.log("true");
       axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
-        props.setUsers(response.data.items);
+        this.props.setUsers(response.data.items);
       });
     }
   };
 
-  return (
-    <div className={classes.body}>
-      <button onClick={getUsers}>Get Users</button>
-      {props.user.map((user) => (
-        <div key={user.id} className={classes.users__card}>
-          <div className={classes.users__ava}>
-            <div className={classes.ava}>
-              <img
-                src={user.photos.small != null ? user.photos.small : userDefaultAva}
-                alt="picture"
-              />
+  render() {
+    return (
+      <div className={classes.body}>
+        <button onClick={this.getUsers}>Get Users</button>
+        {this.props.user.map((user) => (
+          <div key={user.id} className={classes.users__card}>
+            <div className={classes.users__ava}>
+              <div className={classes.ava}>
+                <img
+                  src={user.photos.small != null ? user.photos.small : userDefaultAva}
+                  alt="picture"
+                />
+              </div>
+              <div className={classes.users_subscribe}>
+                {user.followed ? (
+                  <button
+                    className={classes.subscribed}
+                    onClick={() => {
+                      this.props.unSubscribeUser(user.id);
+                    }}
+                  >
+                    Отписаться
+                  </button>
+                ) : (
+                  <button
+                    className={classes.unsubscribed}
+                    onClick={() => {
+                      this.props.subscribeUser(user.id);
+                    }}
+                  >
+                    Подписаться
+                  </button>
+                )}
+              </div>
             </div>
-            <div className={classes.users_subscribe}>
-              {user.followed ? (
-                <button
-                  className={classes.subscribed}
-                  onClick={() => {
-                    props.unSubscribeUser(user.id);
-                  }}
-                >
-                  Отписаться
-                </button>
-              ) : (
-                <button
-                  className={classes.unsubscribed}
-                  onClick={() => {
-                    props.subscribeUser(user.id);
-                  }}
-                >
-                  Подписаться
-                </button>
-              )}
+            <div className={classes.users__desc}>
+              <div className={classes.users__name}>
+                <div>{user.name}</div>
+                <div>{user.status}</div>
+              </div>
+              {/* <div className={classes.users__location}>
+                  <div>{user.userLocation.userCountry}</div>
+                  <div>{user.userLocation.userCity}</div>
+                </div> */}
             </div>
           </div>
-          <div className={classes.users__desc}>
-            <div className={classes.users__name}>
-              <div>{user.name}</div>
-              <div>{user.status}</div>
-            </div>
-            {/* <div className={classes.users__location}>
-              <div>{user.userLocation.userCountry}</div>
-              <div>{user.userLocation.userCity}</div>
-            </div> */}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
